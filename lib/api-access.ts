e3719f865e0b082925ae61abe8bb476097a5a9b5
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-02-11 19:29:58 +0900

    Finished Chaprt 4.

--- lib/api-access.ts
+++ lib/api-access.ts
@@ -3,6 +3,8 @@ import { Recipe } from '@/schemas/schema';
 async function getRecipes(): Promise<Recipe[]> {
   const result = await fetch('http://localhost:4000/recipes')
 
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
   return result.json()
 };
 
